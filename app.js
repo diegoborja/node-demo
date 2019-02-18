ccdbd10b323d8a6fffb13e67a290963fcae600b5
Diego Borja <diego.borja.v@gmail.com>
2019-02-18 14:09:25 -0500

    express

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-const logger = require("./logger.js");
-logger.on("logged", (arg)=>{
-    console.log(arg);
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const courses = [];
+
+app.use(express.json());
+
+app.get("/api/courses", (req, res) => {
+    res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+    let course = courses.find(c=> c[req.params.id]);
+    if (!course) {
+        res.status(404).send(`Course with Id: ${req.params.id} does not exist`);
+        return;
+    } 
+    res.send(course);   
+});
+
+app.post("/api/courses", (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    let validation = Joi.validate(req.body, schema);
+    if (validation.error){
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    let map = {};
+    let id = Date.now();
+    map[id] = {
+        id: id,
+        name: req.body.name
+    }
+    courses.push(map);
+    res.send(map[id]);
 })
-logger.log("Test");
+
+app.listen(port);
